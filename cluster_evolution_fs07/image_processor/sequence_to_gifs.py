ff9b85f2503a8f9bb1839ba14bb5cb2fb63ffdbc
Fred Garcia <fgarcia4@login-2.deepthought2.umd.edu>
2021-10-19 14:14:18 -0400

    gifs for proj density

--- cluster_evolution_fs07/image_processor/sequence_to_gifs.py
+++ cluster_evolution_fs07/image_processor/sequence_to_gifs.py
@@ -2,8 +2,8 @@ import glob
 from PIL import Image
 
 # filepaths
-fp_in = "/homes/fgarcia4/analysis/cluster_evolution_fs07/sequences/new_refine/z_proj_den/*.png"
-fp_out = '/homes/fgarcia4/analysis/cluster_evolution_fs07/image_processor/gifs/z_proj_den.gif'
+fp_in = "/homes/fgarcia4/analysis/cluster_evolution_fs07/sequences/new_refine/y_proj_den/*.png"
+fp_out = '/homes/fgarcia4/analysis/cluster_evolution_fs07/image_processor/gifs/y_proj_den.gif'
 print(fp_in)
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
