314cebcedc9baa2b37b85217b4c5e0908efeef34
fred144 <fgarcia4@umd.edu>
2021-10-26 02:34:56 -0400

    filter age not changeing after each loop

--- cluster_evolution_fs07/clump_tracker_v2.py
+++ cluster_evolution_fs07/clump_tracker_v2.py
@@ -3,17 +3,14 @@ import os
 import yt
 import numpy as np
 #import scipy as sc
-import functools, sys
 from yt.funcs import mylog
 
 mylog.setLevel(40)
 warnings.simplefilter(action = "ignore", category = RuntimeWarning)
-namespace = sys._getframe(0).f_globals
 
 def succ_distance (current, previous):
     dist = np.linalg.norm(np.array(current) - np.array(previous)) 
     return dist 
-
 def code_age_to_yr (all_star_ages, hubble_const, unique = True): 
     r"""
     Returns an array with unique birth epochs in Myr given  
@@ -22,60 +19,50 @@ def code_age_to_yr (all_star_ages, hubble_const, unique = True):
     hubble = ds.hubble_constant  
     Youngest is 0 Myr, all others are relative to the youngest.
     """
-    if unique is True:
+    cgs_yr = 3.1556926e7          # 1yr (in s)
+    cgs_pc = 3.08568e18           # pc (in cm)
+    h_0 =  hubble_const*100 #hubble parameter (km/s/Mpc)
+    h_0_invsec = h_0*1e5/(1e6*cgs_pc) #hubble constant h [km/s Mpc-1] -> [1/sec]
+    h_0inv_yr = 1/h_0_invsec/cgs_yr   #1/h_0 [yr]
+    
+    if unique == True:
         #process to unique birth epochs only
         be_star_processed = np.array(sorted( list( set(all_star_ages.to_ndarray()) ) ) ) 
-        cgs_yr = 3.1556926e7          # 1yr (in s)
-        cgs_pc = 3.08568e18           # pc (in cm)
-        h_0 =  hubble_const*100 #hubble parameter (km/s/Mpc)
-        h_0_invsec = h_0*1e5/(1e6*cgs_pc) #hubble constant h [km/s Mpc-1] -> [1/sec]
-        h_0inv_yr = 1/h_0_invsec/cgs_yr   #1/h_0 [yr]
         star_age_myr = be_star_processed * h_0inv_yr/1e6
         relative_ages = star_age_myr - star_age_myr.min()
     else: 
-        code_ages_unfiltered = all_star_ages
-        cgs_yr = 3.1556926e7          # 1yr (in s)
-        cgs_pc = 3.08568e18           # pc (in cm)
-        h_0 =  hubble_const*100 #hubble parameter (km/s/Mpc)
-        h_0_invsec = h_0*1e5/(1e6*cgs_pc) #hubble constant h [km/s Mpc-1] -> [1/sec]
-        h_0inv_yr = 1/h_0_invsec/cgs_yr   #1/h_0 [yr]
-        star_age_myr = code_ages_unfiltered * h_0inv_yr/1e6
+        all_stars  =  np.array(list( all_star_ages.to_ndarray()) )
+        star_age_myr = all_stars * h_0inv_yr/1e6
         relative_ages = star_age_myr - star_age_myr.min()
-        #print(relative_ages)
+    #print(relative_ages)
     return relative_ages
 
 def clump_tracker(ds, birth_epochs, plot_object, width): 
-    #print(birth_epochs)
+    
     for clump_num, be in enumerate(birth_epochs):
         
-        clump_name = 'clump_' + str(clump_num)
+        #clump_name = 'clump_' + str(clump_num)
         
-       # @yt.particle_filter(requires=['particle_birth_epoch'], filtered_type='star')
+        @yt.particle_filter(requires=['particle_birth_epoch'], filtered_type='star')
         def clump(pfilter, data):
             h = ds.hubble_constant
-            
             code_ages = data[pfilter.filtered_type, 'particle_birth_epoch'] 
-            
             relative_ages = code_age_to_yr(all_star_ages=code_ages, 
                                            hubble_const=h,
                                            unique=False)
-            
-            filter = np.logical_and(np.isclose(relative_ages, be), relative_ages >= 0)
+            print(relative_ages)
+            print(be)
+            filter = relative_ages != be
+            #filter = 
             return filter
-        
-        namespace['clump_%s'%str(be)] = functools.partial(clump)
-        
-        yt.add_particle_filter(clump_name, 
-                                function=namespace['clump_%s'%str(be)], 
-                                filtered_type='star', 
-                                requires=['particle_birth_epoch'],
-                                )
-        ds.add_particle_filter(clump_name) 
-    
-    
-    
-        
-
+        # yt.add_particle_filter( 
+        #     "clump1", function = clump1, filtered_type="star", requires=["particle_birth_epoch"]
+        #     )
+        ds.add_particle_filter('clump') 
+        # 
+        plot_object.annotate_particles( 
+            width=width, ptype='clump', p_size=10.0, marker='.', col=np.random.rand(3,1).T  
+            ) 
         
 #data directory/info file
 datadir = os.path.expanduser(
@@ -148,9 +135,8 @@ for loop_num, output_num in enumerate(range(start_step, end_step + 1)) :
 
     # particle clumps by age 
     unique_birth_epochs = code_age_to_yr(raw_birth_epochs, current_hubble)
-    print(unique_birth_epochs)
     
-    # keep center of plots relatively stable
+    #keep center of plots relatively stable
     if loop_num == 0:
         max_density_coords.append(max_density_coord)
     
@@ -178,7 +164,7 @@ for loop_num, output_num in enumerate(range(start_step, end_step + 1)) :
         print('> using old center at {}'. format(center)) 
         
         
-    
+    print('annotating', np.array(be_star).size, 'star particles')
     if pos_SFCs.size > 0:
 #         first_sfc_center = pos_SFCs[0]  
 #         p = yt.SlicePlot(ds, 'z', "density", width = width, center = first_sfc_center) #turn this on to track center of sfc
@@ -198,58 +184,7 @@ for loop_num, output_num in enumerate(range(start_step, end_step + 1)) :
             
     #p.annotate_particles(width=width, ptype='star', p_size=10.0,marker='.',col='r')    
     
-    #print('> annotating', np.array(be_star).size, 'star particles')
-    #clump_tracker(ds=ds, birth_epochs=unique_birth_epochs, plot_object=p, width=width)
-    
-
-    for clump_num, be in enumerate(unique_birth_epochs):
-        
-        clump_name = 'clump_' + str(clump_num)
-        
-       # @yt.particle_filter(requires=['particle_birth_epoch'], filtered_type='star')
-        def clump(pfilter, data):
-            h = ds.hubble_constant
-            code_ages = data[pfilter.filtered_type, 'particle_birth_epoch'] 
-            relative_ages = code_age_to_yr(all_star_ages=code_ages, 
-                                           hubble_const=h,
-                                           unique=False)
-            print(relative_ages)
-            print(be)
-            filter = np.logical_and(np.isclose(relative_ages, be), relative_ages >= 0)
-            return filter
-        
-        namespace['clump_%s'%str(be)] = functools.partial(clump)
-        
-        yt.add_particle_filter(clump_name, 
-                                function=namespace['clump_%s'%str(be)], 
-                                filtered_type='star', 
-                                requires=['particle_birth_epoch'],
-                                )
-        ds.add_particle_filter(clump_name) 
-    
-    
-    p.annotate_particles(width=width, 
-    ptype= 'clump_0', 
-    p_size=10.0, marker='.', 
-    col=np.random.rand(3,1).T  
-    ) 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    clump_tracker(ds=ds, birth_epochs=unique_birth_epochs , plot_object=p, width=width)
     
     p.annotate_timestamp(corner='lower_left', 
                          time_format='t = {time:.2f} {units}', 
