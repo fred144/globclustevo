1d533e02590c344e1d235a2ff9bfbdf601a4d2f1
fred144 <fgarcia4@umd.edu>
2021-11-02 15:49:37 -0400

    plot time series check

--- cluster_evolution_fs07/plots.py
+++ cluster_evolution_fs07/plots.py
@@ -7,9 +7,15 @@ be = np.array(
      69.34419314, 70.17392563, 70.19719182, 70.2027757,  70.30222356, 70.41787201,
      70.79423501, 88.28128775, 89.38194823, 90.51360277, 95.28010249, 98.17317232]
     )
+birth_epochs = be + 339.562
+
+   
+    
 plt.figure(figsize = (12,8))
 plt.scatter(time_series_data[:,1], time_series_data[:,2])
-
+for birth_epoch in birth_epochs:
+    plt.axvline(x=birth_epoch, color = 'k', linestyle='--') 
 plt.xlabel('Time (Myr)')
 plt.ylabel('Total Pop 2 Mass (Msun)')
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.legend()
