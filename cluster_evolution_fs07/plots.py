64e2567e1bcb672fc15dab03178682ca0d307137
fred144 <fgarcia4@umd.edu>
2021-11-02 14:45:40 -0400

    plots with BE

--- cluster_evolution_fs07/plots.py
+++ cluster_evolution_fs07/plots.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 time_series_data = np.loadtxt('timeseriesdata.txt')
-
+be = np.array(
+    [ 0., 18.52773269, 35.60895952, 68.6774688,  68.96389576, 69.09889334,
+     69.34419314, 70.17392563, 70.19719182, 70.2027757,  70.30222356, 70.41787201,
+     70.79423501, 88.28128775, 89.38194823, 90.51360277, 95.28010249, 98.17317232]
+    )
 plt.figure(figsize = (12,8))
 plt.scatter(time_series_data[:,1], time_series_data[:,2])
 
