b6aac37a89cd953670685560701b739793634450
fred144 <fgarcia4@umd.edu>
2021-12-21 14:08:45 -0500

    luminosity map

---
+++ luminosity_mapping/star_heat_map.py
@@ -0,0 +1,8 @@
+from cluster_evolution_fs07.macros import code_age_to_yr
+import numpy as np
+import os
+import yt
+
+pop_2_data = np.loadtxt(r'star_positions_out400.txt') 
+x_pos = pop_2_data[:,1]
+y_pos = pop_2_data[:,2] 
