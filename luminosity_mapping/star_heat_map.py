44a6e8af46f6f95a31cc33648367f0640852bd69
Fred <fgarcia4@umd.edu>
2022-01-16 00:38:35 -0500

    rotation test

--- luminosity_mapping/star_heat_map.py
+++ luminosity_mapping/star_heat_map.py
@@ -10,6 +10,7 @@ import matplotlib as mpl
 import matplotlib.patches as patches
 from scipy.ndimage.filters import gaussian_filter
 from matplotlib.colors import LogNorm 
+from scipy.spatial.transform import Rotation as R 
 
 def look_up_table(stellar_ages, look_up_times, look_up_lumi):
     residuals = np.abs(look_up_times - stellar_ages[:, np.newaxis]) 
@@ -31,83 +32,96 @@ lum_imf_2_35_m_up_30msun = 10**data[:,3]
 mpl.rc('font', family='serif') 
 
 directory = r"./pop_2_data/"
-for frame,entry in enumerate(sorted(os.listdir(directory)), start=115):
+files = sorted(os.listdir(directory))  
+for frame,file_name in enumerate(files[-2:-1], start=115):
 
-    file_name = entry
-    #print(file_name)
+    print(file_name)
     time_str = file_name[10:16].replace('_','.') #in myr
     time = float(time_str)
-    print(file_name)
+    #print(file_name)
 
     pop_2_data = np.loadtxt('./pop_2_data/' + file_name) 
     birth_epochs = pop_2_data[:,0] *1e6 
     ages = pop_2_data[:,1] *1e6 
     # If the age <10^6 yr, set the age to 10^6 year.
     ages [ages < 1e6 ] = 1e6
-    x_pos = pop_2_data[:,2]
-    y_pos = pop_2_data[:,3] 
-    z_pos = pop_2_data[:,4]
     
-    stellar_lums = look_up_table(ages, lookuptime, lum_imf_2_35_m_up_100msun) 
+    # x_pos = pop_2_data[:,2]
+    # y_pos = pop_2_data[:,3] 
+    # z_pos = pop_2_data[:,4]
+    rotation_interval = np.arange(0,2,.005) 
     
-    lums, xedges, yedges = np.histogram2d(
-        x_pos, 
-        y_pos, 
-        bins=2000, 
-        weights=stellar_lums*1e-5, 
-        normed=False, 
-        range= [[-200, 200], [-200, 200]]
-    )
-    lums = lums.T
-    
-    fig = plt.figure(figsize=(12,10)) 
-    ax = fig.add_subplot(111, facecolor=cm.magma(0))
-    rectbin = plt.imshow(
-               lums, 
-               cmap='inferno',
-               interpolation='gaussian', 
-               origin='lower',
-               extent=[-200, 200,-200, 200],
-               norm=LogNorm(vmin=3e+32, vmax=3e+36)
-               )
-    # annotate with time 
-    ax.text(
-        -150, 
-        -180, 
-        't = %.2f Myr'%(time), 
-        size=12, 
-        ha='center', 
-        va='center', 
-        color='white')
-    # and color bar 
-    cbar = plt.colorbar(
-        rectbin, 
-        pad = 0,
-        aspect=40
+    for i,angle_multiple in enumerate(rotation_interval):
+        star_positions = pop_2_data[:,2:5] 
+        
+        r = R.from_rotvec(angle_multiple*np.pi * np.array([0, 1, 0]))
+        rotation_matrix = r.as_matrix()
+        
+        star_positions = np.dot(star_positions, rotation_matrix .T)
+        
+        stellar_lums = look_up_table(ages, lookuptime, lum_imf_2_35_m_up_100msun) 
+        
+        lums, xedges, yedges = np.histogram2d(
+            star_positions[:,0], 
+            star_positions[:,1], 
+            bins=2000, 
+            weights=stellar_lums*1e-5, 
+            normed=False, 
+            range= [[-200, 200], [-200, 200]]
         )
-    cbar.set_label(
-        label=r'z Projected Monochromatic Luminosity at $\lambda = 150$ nm', size=12
-        )
-    # and scale bar 
-    rect = patches.Rectangle(   
-            xy=(75, 180), 
-            width=100, 
-            height=2, 
-            linewidth=0, 
-            edgecolor='white', 
-            facecolor='white')
-    ax.add_patch(rect)
-    ax.text(
-        125, 190, '100 pc', size=12, ha='center', va='center', color='white'
-        )
-    ax.set_axis_off()
-    ax.add_artist(ax.patch)
-    ax.patch.set_zorder(-1) 
-    
-    plt.savefig('./sequences/z_projection/z_lums_{}_{}.png'.format(
-        str(frame).zfill(3), str(time).ljust(6, '0').replace('.','_') ), 
-        dpi=200,
-        bbox_inches='tight',
-        pad_inches=0.05
-        )
-    #plt.show() 
+        lums = lums.T
+        
+        fig = plt.figure(figsize=(12,10)) 
+        ax = fig.add_subplot(111, facecolor=cm.magma(0))
+        rectbin = plt.imshow(
+                   lums, 
+                   cmap='inferno',
+                   interpolation='gaussian', 
+                   origin='lower',
+                   extent=[-200, 200,-200, 200],
+                   norm=LogNorm(vmin=3e+32, vmax=3e+36)
+                   )
+        # annotate with time 
+        ax.text(
+            -150, 
+            -180, 
+            't = %.2f Myr'%(time), 
+            size=12, 
+            ha='center', 
+            va='center', 
+            color='white')
+        # and color bar 
+        cbar = plt.colorbar(
+            rectbin, 
+            pad = 0,
+            aspect=40
+            )
+        cbar.set_label(
+            label=r'z Projected Monochromatic Luminosity at $\lambda = 150$ nm', size=12
+            )
+        # and scale bar 
+        rect = patches.Rectangle(   
+                xy=(75, 180), 
+                width=100, 
+                height=2, 
+                linewidth=0, 
+                edgecolor='white', 
+                facecolor='white')
+        ax.add_patch(rect)
+        ax.text(
+            125, 190, '100 pc', size=12, ha='center', va='center', color='white'
+            )
+        ax.set_axis_off()
+        ax.add_artist(ax.patch)
+        ax.patch.set_zorder(-1) 
+        
+        plt.savefig('./test_sequence/z_projection/z_lums_{}_{}_{}.png'.format(
+            str(frame).zfill(3), 
+            str(time).ljust(6, '0').replace('.','_'),
+            str(i).zfill(4), 
+            ), 
+            dpi=200,
+            bbox_inches='tight',
+            pad_inches=0.05
+            )
+        plt.close() 
