44889baeb789a176b04f3393e015ba9452369a5e
Fred <fgarcia4@umd.edu>
2022-03-27 19:25:12 -0400

    data scraper

---
+++ sim_log_files/scraper.py
@@ -0,0 +1,26 @@
+import os 
+import sys
+import shutil
+
+lustre = "/lustre/fgarcia4/ramses/dwarf/data/cluster_evolution" 
+#lustre = "/home/fabg/cosm_test_data"
+for path, subdirs, files in os.walk(lustre):
+    for name in files:
+       
+        #print(name)
+        #print(os.path.join(path, name))
+        if "log" in name and "old" not in path: 
+            full_path_to_files = os.path.join(path, name)
+            test = os.path.basename(full_path_to_files)
+            sim_name = os.path.basename(os.path.dirname(full_path_to_files))
+            save_folder_name = "./" + sim_name
+            if not os.path.exists(save_folder_name):
+                print("# Creating new directory: ",  save_folder_name)
+                os.makedirs( save_folder_name)
+                
+            shutil.copyfile(src=full_path_to_files, dst=save_folder_name)
+            
+        
+           # print(test)
+            print(full_path_to_files )
+        
