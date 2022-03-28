d766cf66f3f143d4f5760e03f031b8300e90f72f
Fred <fgarcia4@umd.edu>
2022-03-27 22:12:50 -0400

    incomplete latest simulation status part of scraper

--- sim_log_files/scraper.py
+++ sim_log_files/scraper.py
@@ -1,26 +1,123 @@
-import os 
+"""
+logfile scraper for lustre data 
+"""
+import numpy as np
+import os
 import sys
 import shutil
+import yt
 
-lustre = "/lustre/fgarcia4/ramses/dwarf/data/cluster_evolution" 
-#lustre = "/home/fabg/cosm_test_data"
+
+def snap_shot_info(sim_output_abs_dir, save_path):
+    # output_{output_num:05}/info_{output_num:05}.txt"
+    infofile = os.path.abspath(sim_output_abs_dir)
+    print("# Reading in", infofile)
+
+    # read fields explicitly, not recognized by YT from this ver of RAMSES
+    cell_fields = [
+        "Density",
+        "x-velocity",
+        "y-velocity",
+        "z-velocity",
+        "Pressure",
+        "Metallicity",
+        "dark_matter_density",
+        "xHI",
+        "xHII",
+        "xHeII",
+        "xHeIII",
+    ]
+    epf = [
+        ("particle_family", "b"),
+        ("particle_tag", "b"),
+        ("particle_birth_epoch", "d"),
+        ("particle_metallicity", "d"),
+    ]
+
+    # read in RAMSES data set
+    ds = yt.load(infofile, fields=cell_fields, extra_particle_fields=epf)
+    redshft = ds.current_redshift
+    current_time = float(ds.current_time.in_units("Myr"))
+
+    ad = ds.all_data()
+
+    # get indivudual masses
+    dark_matter = ad["DM", "particle_mass"].in_units("Msun")
+    pop_ii = ad["star", "particle_mass"].in_units("Msun")
+    # living  pop three stars
+    pop_iii = ad["POPIII", "particle_mass"].in_units("Msun")
+    # Pop III stars taking place SNe
+    sn = ad["supernova", "particle_mass"].in_units("Msun")
+    # Pop III stars after SNe
+    dead = ad["dead", "particle_mass"].in_units("Msun")
+    # Pop III remnant BHs
+    black_hole = ad["BH", "particle_mass"].in_units("Msun")
+    # gas
+    m_gas = ad["gas", "mass"].in_units("Msun").sum()
+
+    # total mass
+    m_dark_matter = dark_matter.sum()
+    m_pop_ii = pop_ii.sum()
+    m_pop_iii = pop_iii.sum()
+    m_sn = sn.sum()
+    m_dead = dead.sum()
+    m_black_hole = black_hole.sum()
+
+    # [redshift, current_time, dm, pop2, pop3, sn, dead, BH, gas mass]
+    tot_masses = np.array(
+        [
+            redshft,
+            current_time,
+            m_dark_matter,
+            m_pop_ii,
+            m_pop_iii,
+            m_sn,
+            m_dead,
+            m_black_hole,
+            m_gas,
+        ]
+    )
+    header = (
+        "\t\t z \t\t SimTime[Myr] \t DM[Msun] \t pop2[Msun] \t pop3[Msun]"
+        " \t SN[Msun] \t Dead[Msun] \t BH[Msun] \t Gas [Msun]"
+    )
+    np.savetxt(fname=save_path + "latest_sim_stats.txt", X=tot_masses, header=header)
+
+
+lustre = "/lustre/fgarcia4/ramses/dwarf/data/cluster_evolution"
+# lustre = "/home/fabg/cosm_test_data"
 for path, subdirs, files in os.walk(lustre):
-    for name in files:
-       
-        #print(name)
-        #print(os.path.join(path, name))
-        if "log" in name and "old" not in path: 
-            full_path_to_files = os.path.join(path, name)
-            test = os.path.basename(full_path_to_files)
+    # path is lustre simulation run names, and output directories
+    for file_name in files:
+        # if it is a log file and not from old runs
+        if "log" in file_name and "old" not in path:
+            # full path to log files
+            full_path_to_files = os.path.join(path, file_name) 
+            
+            # get simulation run names; e.g., fs07_refine, fs035_ms10
+            # last entry of directory abs path is the directory/ simulation run itself
             sim_name = os.path.basename(os.path.dirname(full_path_to_files))
             save_folder_name = "./" + sim_name
-            if not os.path.exists(save_folder_name):
-                print("# Creating new directory: ",  save_folder_name)
-                os.makedirs( save_folder_name)
-                
-            shutil.copy(src=full_path_to_files, dst=save_folder_name)
             
-        
-           # print(test)
-            print(full_path_to_files )
-        
+            # folder for saving in the script directory
+            if not os.path.exists(save_folder_name):
+                print("# Creating new directory:", save_folder_name)
+                os.makedirs(save_folder_name)
+            
+            # copy over the log file 
+            shutil.copy(src=full_path_to_files, dst=save_folder_name)
+
+            print("# Copied: ", full_path_to_files)
+            
+# get latest sim results 
+sim_runs = [x for x in os.listdir(lustre) if "old" not in x]
+
+for simrun in sim_runs: 
+    sim_folder = os.path.join(lustre,  simrun) 
+    output_folders = sorted(os.listdir(sim_folder) )
+    output_folders = [x for x in output_folders if "output_" in x]
+    latest_snapshots_abs_dir = os.path.join(sim_folder,output_folders[-1]) 
+    print(latest_snapshots_abs_dir )
+    
+
+    
