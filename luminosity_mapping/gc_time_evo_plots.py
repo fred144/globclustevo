5518ebdb3ef661be0b5a014ee5622729f37826f9
fred144 <fgarcia4@umd.edu>
2022-04-04 00:09:02 -0400

    imf

--- luminosity_mapping/gc_time_evo_plots.py
+++ luminosity_mapping/gc_time_evo_plots.py
@@ -3,6 +3,75 @@ import numpy as np
 import os
 import sys
 
+# =============================================================================
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# def read_cat(filen):
+#    t=[]
+#    x=[]
+#    y=[]
+#    V=[]
+#    I=[]
+#
+#    f=open(filen, 'r')
+#    i=0
+#    for line in f:
+#      if (i >0):
+#         t0=float(line.split()[2])
+#         x1=float(line.split()[4])
+#         y1=float(line.split()[5])
+#         V1=float(line.split()[8])
+#         I1=float(line.split()[9])
+#         t.append(t0)
+#         x.append(x1)
+#         y.append(y1)
+#         V.append(V1)
+#         I.append(I1)
+#      i=i+1
+#
+#    print('total lines',i)
+#    t=np.array(t)
+#    x=np.array(x)
+#    y=np.array(y)
+#    V=np.array(V)
+#    I=np.array(I)
+#    return t,x,y,V,I
+#
+# def f_star(n_H,mass,met):
+# 	#n_crit=n_H*0+1.e2/1.8
+# 	n_crit=n_H*0+1.e2/1.3
+# 	f_s=4.e-3*(met/1e-3)**0.5*(mass/1.e4)**0.4*(n_H/n_crit+1.0)**(0.91)
+# 	f_s=np.where(f_s<0.9,f_s,0.9)
+# 	return f_s
+#
+# z,ra,mass,n_H,met=read_cat('logSFC.txt')
+#
+# fig, axs= plt.subplots(2, 2)
+#
+# axs[0,0].loglog(met,n_H,'o')
+# axs[0,0].set(xlabel='met',ylabel='$n_H$')
+#
+# f_s=f_star(n_H,mass,met)
+#
+# axs[0,1].loglog(mass,f_s,'o')
+# axs[0,1].set(xlabel='mass',ylabel='$f_*$')
+#
+# axs[1,0].loglog(n_H,f_s,'o')
+# axs[1,0].set(xlabel='$n_H$',ylabel='$f_*$')
+#
+# h0,bins=np.histogram(np.log10(mass),bins=10)
+# h1,bins1=np.histogram(np.log10(mass),bins=20)
+# #h1,bins=np.histogram(np.log10(mass[0:50]),bins=10)
+# binc=(bins[1:]+bins[:-1])/2.0
+# binc1=(bins1[1:]+bins1[:-1])/2.0
+#
+# axs[1,1].plot(binc,np.log10(h0),'o-')
+# axs[1,1].plot(binc1,np.log10(h1),'o-')
+# axs[1,1].set(xlabel='mass',ylabel='dn/dlogM')
+#
+# plt.show()
+# =============================================================================
 # plt.figure(figsize = (8,8), dpi=200)
 # plt.hist(core_masses, bins=np.geomspace(core_masses.min(), core_masses.max(),10), histtype='step',  fill=False)
 # plt.xscale('log')
@@ -10,11 +79,56 @@ import sys
 # https://matplotlib.org/stable/gallery/lines_bars_and_markers/scatter_with_legend.html
 
 
+def mass_function(masses, t_sim, num_bins, m_core=None):
+
+    bins = np.geomspace(np.min(masses), np.max(masses), num=num_bins, endpoint=True)
+    count, bin_edges = np.histogram(masses, bins=bins)
+    right_edges = bin_edges[1:]
+    left_edges = bin_edges[:-1]
+    bin_ctrs = 0.5 * (left_edges + right_edges)
+
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=200)
+    # ax.errorbar(bin_ctrs, count, fmt="-o")
+    ax.hist(masses, bins, histtype="step", linewidth=4, alpha=0.5, label=r"$M_{trunc}$")
+
+    if m_core is not None:
+
+        count, bin_edges = np.histogram(m_core, bins=bins)
+        right_edges = bin_edges[1:]
+        left_edges = bin_edges[:-1]
+        bin_ctrs = 0.5 * (left_edges + right_edges)
+
+        # ax.errorbar(bin_ctrs, count, fmt="-o")
+        ax.hist(
+            m_core, bins, histtype="step", linewidth=4, alpha=0.5, label=r"$M_{core}$"
+        )
+
+    ax.set_title(r"$t_{{sim}}$ = {} Myr".format(t_sim), fontsize=18)
+
+    ax.set_xlabel(r"$ M$ / M$_{\odot} $", fontsize=18)
+    ax.set_ylabel(r"dN / d$\log \left( \frac{M}{M_{\odot}} \right)$", fontsize=18)
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_ylim(top=100)
+    ax.legend(fontsize=18, loc="upper left")
+
+    # another type
+    # fig, ax = plt.subplots(figsize=(8, 8), dpi=200)
+    # ax.bar(
+    #     np.log10(bin_ctrs), np.log10(count), width=np.ptp(np.log10(bin_ctrs)) / num_bins
+    # )
+    # ax.errorbar(np.log10(bin_ctrs), np.log10(count), fmt="-ok")
+    # ax.set_xlabel(r"$\log_{10} \left(  M / M_{\odot} \right)$", fontsize=18)
+    # ax.set_ylabel(r"dN / d$\log_{10}  \left( \frac{M}{M_{\odot}} \right)$", fontsize=18)
+    # ax.set_ylim(top=2)
+    # # ax.set_yscale("log")
+
+
 def bubble_plot(masses, core_radii, ages, current_time):
     core_diameter = core_radii * 2
 
     colors = np.random.uniform(size=masses.size)
-    norm = 1.5
+    norm = 5
     # map to differnt sizes for better plotting
     core_diameter_per_size = (500 * core_diameter) / norm
 
@@ -42,15 +156,15 @@ def bubble_plot(masses, core_radii, ages, current_time):
         *scatter.legend_elements(**legend_properties),
         loc="lower left",
         title="$d_{core}$ (pc)",
-        title_fontsize=16,
+        title_fontsize=18,
         fontsize=15,
     )
     plt.grid(visible=True)
 
-    ax.set_title(r"$t_{{sim}}$ = {} Myr".format(current_time), fontsize=16)
-    ax.set_ylabel(r"GC Truncation Mass ($M_{\odot}$)", fontsize=16)
-    ax.set_xlabel(r"Formation Time (Myr)", fontsize=16)
-    ax.set_xlim(240, 500)
+    ax.set_title(r"$t_{{sim}}$ = {} Myr".format(current_time), fontsize=18)
+    ax.set_ylabel(r"GC Truncation Mass ($M_{\odot}$)", fontsize=18)
+    ax.set_xlabel(r"Formation Time (Myr)", fontsize=18)
+    ax.set_xlim(300, 500)
     ax.set_ylim(10, 1e6)
     ax.set_yscale("log")
 
@@ -62,7 +176,7 @@ if __name__ == "__main__":
     data_directory = r"./gc_profiles/"
     # enable discrete selection of time range based on snapshot number
 
-    files = sorted(os.listdir(data_directory))  # [-2:-1]  [300:400:2]
+    files = sorted(os.listdir(data_directory))[-2:-1]  # [300:400:2]
     # strt_idx = [i for i, s in enumerate(files) if strt_snapshot in s][0]
     # end_idx = [i for i, s in enumerate(files) if end_snapshot in s][0]
     # filtered_files = files [strt_idx:end_idx:1]
@@ -103,7 +217,12 @@ if __name__ == "__main__":
                 current_time=t_myr,
             )
 
-        except:
+            mass_function(
+                masses=gc_out_masses, t_sim=t_myr, num_bins=10, m_core=gc_m_core
+            )
+
+        except Exception as e:
+            print(e)
             print("> Missing info file:", data_file)
             pass
 
