ecf95514293109e31bcc140dd3838c7d74f43562
Fred <fgarcia4@umd.edu>
2022-04-18 23:43:40 -0400

    fof halo finder analysis

--- luminosity_mapping/profiler.py
+++ luminosity_mapping/profiler.py
@@ -222,12 +222,12 @@ def run_profiler(file_name, proj_width, gc_radii, lum_map_bins, **kwargs):
 if __name__ == "__main__":
     data_directory = r"./pop_2_data/"
     # enable discrete selection of time range based on snapshot number
-    strt_snapshot = "00710"
-    end_snapshot = "00810"
+    strt_snapshot = "00300"
+    end_snapshot = "00740"
     files = sorted(os.listdir(data_directory))  # [-2:-1]  [300:400:2]
     strt_idx = [i for i, s in enumerate(files) if strt_snapshot in s][0]
     end_idx = [i for i, s in enumerate(files) if end_snapshot in s][0]
-    filtered_files = files[strt_idx : end_idx + 1 : 1]
+    filtered_files = files[strt_idx : end_idx + 1 : 5]
 
     for file_name in filtered_files:
 
