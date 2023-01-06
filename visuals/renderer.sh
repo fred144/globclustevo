9270d09378f0cb7f84e1306e71ac3a3481e1bb03
Fred Garcia <fgarcia4@login-2.zaratan.umd.edu>
2023-01-05 21:07:02 -0500

    rendering

--- visuals/renderer.sh
+++ visuals/renderer.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 #SBATCH -J gas_renderer
-#SBATCH --ntasks=2
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=5990
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=3999
 #SBATCH -t 24:00:00
 
-# needed for bash in deepthought2 (cf. https://www.glue.umd.edu/hpcc/help/jobs.html#basic)
 #. ~/.bashrc
 . ~/.profile
 
 # designed to be submitted/ ran in the script directory
 export LANG=en_US
-#module unload intel
-#module unload intel-mpi
-#module load gcc
-#module load openmpi
+module purge
 module load python
 module load gcc
 module load openmpi
+# activate py environment
 source ~/scratch/master/bin/activate
-
-
-#SCRIPT="/lustre/fgarcia4/glob_clust_evo/visuals/fs070_gas_panel_projection.py"
+ 
 SCRIPT= "/scratch/zt1/project/ricotti-prj/user/fgarcia4/globclustevo/visuals/low-sfe.py"
-#cd /scratch/zt1/project/ricotti-prj/user/fgarcia4/globclustevo/visuals/
-
+# also tried
+# cd /scratch/zt1/project/ricotti-prj/user/fgarcia4/globclustevo/visuals/
+# SCRIPT = "./low-sfe.py"
 # number of cores specified above
-#mpirun python3 $SCRIPT  2>&1 
-mpirun $SCRIPT  2>&1 
+mpirun python3 $SCRIPT  #2>&1 
